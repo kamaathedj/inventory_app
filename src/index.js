30f1cb648712651ac35d93a17cee8daaf88f6994
kamaathedj <kamaudavikiruku@gmail.com>
2020-05-15 10:21:03 +0300

    :twisted_rightwards_arrows: restructuring the code

--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
-const middleware = require('./middlewares');
-const routes = require('./api/logs');
+
 require('dotenv').config();
+const app = require('./app');
 
 const port = process.env.port || 3000;
-const app = express();
-
-app.use(morgan('common'));
-app.use(helmet());
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-}));
-app.use(express.json());
-
-app.use('/api', routes);
 
 
-// this should be the last route its is a not found page
-app.use(middleware.notFound);
 
-// error handling route
-app.use(middleware.errorHandler);
 
 
 app.listen(port, () => {
