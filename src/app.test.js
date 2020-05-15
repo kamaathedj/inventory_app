8ce579a22af268364540ae084c3dbe85eacd5a26
kamaathedj <kamaudavikiruku@gmail.com>
2020-05-15 11:43:50 +0300

    test for not found route

--- src/app.test.js
+++ src/app.test.js
@@ -3,12 +3,21 @@ const supertest = require('supertest');
 const app = require('./app');
 
 describe('App', () => {
-  it('it should respond with a welcome message at the base of the Api', async () => {
+  it('should respond with a welcome message at the base of the Api', async () => {
     const response = await supertest(app)
       .get('/')
       .expect('Content-Type', /json/)
       .expect(200);
 
     expect(response.body.message).toEqual('Home inventory api');
+  });
+
+  it('should return an error', async () => {
+    const response = await supertest(app)
+      .get('/api/todo')
+      .expect('Content-Type', /json/)
+      .expect(404);
+
+    expect(response.body.message).toEqual('Not Found - /api/todo');
   });
 });
