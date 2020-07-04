d3ff1b3c2f5eaafff748a4ae96dc3fdb7b8036d7
kamaathedj <kamaudavikiruku@gmail.com>
2020-07-04 10:21:15 +0300

    minor fix

--- src/api/countries/countries.test.js
+++ src/api/countries/countries.test.js
@@ -10,6 +10,7 @@ describe('GET /countries', () => {
       .get('api/v1/countries')
       .expect('Content_Type', /json/)
       .expect(200);
-    expect(response.body).isEmpty();
+    // eslint-disable-next-line no-unused-expressions
+    expect(response.body).isEmpty;
   });
 });
