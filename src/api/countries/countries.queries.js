bb4d50766e362581a6a844606409181f93c542ed
kamaathedj <kamaudavikiruku@gmail.com>
2020-07-06 15:16:51 +0300

    :test: test some few methods

--- src/api/countries/countries.queries.js
+++ src/api/countries/countries.queries.js
@@ -3,6 +3,9 @@ const tableNames = require('../../tableNames');
 
 module.exports = {
   find() {
-    return db(tableNames.country);
+    return db(tableNames.country).select('id', 'name');
+  },
+  get() {
+    return db(tableNames.country).max('name');
   },
 };
