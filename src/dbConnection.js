4a96189a5436677d5c0a193cb893bc08ea07558e
kamaathedj <kamaudavikiruku@gmail.com>
2020-07-09 15:53:46 +0300

    added objection model to the knex connection

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -1,9 +1,12 @@
 const knex = require('knex');
+const { Model } = require('objection');
 const knexConfig = require('../knexfile');
+
 
 const environment = process.env.NODE_ENV || 'development';
 
 const connectionConfig = knexConfig[environment];
 
 const connection = knex(connectionConfig);
+Model.knex(connection);
 module.exports = connection;
